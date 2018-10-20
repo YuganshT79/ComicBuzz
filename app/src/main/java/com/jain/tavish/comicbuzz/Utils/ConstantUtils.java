5feb5ee219ce6398b90247084c971ea7322dfc8f
Yugansh Tyagi <yugansh39@gmail.com>
2018-10-20 15:43:29 +0530

    Improved Issue row item

--- app/src/main/java/com/jain/tavish/comicbuzz/Utils/ConstantUtils.java
+++ app/src/main/java/com/jain/tavish/comicbuzz/Utils/ConstantUtils.java
@@ -7,7 +7,7 @@ public class ConstantUtils {
     public static final int NAV_ITEM_CHARACTERS = 2;
     public static final int NAV_ITEM_MY_FAVS = 3;
 
-    public static final String API_KEY = "";
+    public static final String API_KEY = "477a13460566c18ec52db6929a21d6f74252c7dd";
 
     public static final String BUNDLE_ACTIVITY_NAME = "activityName";
     public static final String BUNDLE_ID = "issueID";
